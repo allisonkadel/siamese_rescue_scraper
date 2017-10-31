c2b818b0c55756402fa859e7189d298f2b297f36
Ally Kadel <akadel01@gmail.com>
2017-10-31 18:56:18 +0000

    create .menu method to interact with user

--- lib/siamese_rescue_scraper/cli.rb
+++ lib/siamese_rescue_scraper/cli.rb
@@ -3,15 +3,31 @@
 class SiameseRescueScraper::CLI
 
   def call
-    puts "Siamese Cats Available:"
     list_cats
+    menu
   end
 
   def list_cats
+    puts "Siamese Cats Available:"
     puts "1 Koda - Virginia Center - 9 Yrs 6 Mon "
     puts "2 Maya - Florida - 9 Yrs 4 Mon"
-    puts "3 Max, Virginia - 10 Yrs 5 Mon"
+    puts "3 Max - Virginia - 10 Yrs 5 Mon"
     puts "4 Powder - North Carolina - 3 Yrs 2 Mon"
+  end
+
+  def menu
+    puts "Enter the number of the cat you'd like more info on. Type 'exit' to leave program."
+    input = gets.strip
+      if input == "1"
+        puts "More info on Koda..."
+      elsif input == "2"
+        puts "More info on Maya..."
+      elsif input == "3"
+        puts "More info on Max..."
+      elsif input == "4"
+        puts "More info on Powder..."
+      end
+    menu unless input == "exit"
   end
 
 end
