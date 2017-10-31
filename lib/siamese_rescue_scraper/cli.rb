cc620b8ce529a0e5855aca8281230b9d71d0658f
Ally Kadel <akadel01@gmail.com>
2017-10-31 18:37:41 +0000

    create .list_cats method for CLI class

--- lib/siamese_rescue_scraper/cli.rb
+++ lib/siamese_rescue_scraper/cli.rb
@@ -4,6 +4,14 @@ class SiameseRescueScraper::CLI
 
   def call
     puts "Siamese Cats Available:"
+    list_cats
+  end
+
+  def list_cats
+    puts "1 Koda - Virginia Center - 9 Yrs 6 Mon "
+    puts "2 Maya - Florida - 9 Yrs 4 Mon"
+    puts "3 Max, Virginia - 10 Yrs 5 Mon"
+    puts "4 Powder - North Carolina - 3 Yrs 2 Mon"
   end
 
 end
