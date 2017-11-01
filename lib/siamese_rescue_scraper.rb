483f48a59ced8851ba922714a5d2f1d566c28e08
Ally Kadel <akadel01@gmail.com>
2017-11-01 01:05:03 +0000

    create SiameseRescueScraper::Cat class

--- lib/siamese_rescue_scraper.rb
+++ lib/siamese_rescue_scraper.rb
@@ -1,3 +1,4 @@
 #This file acts as environment file
-require_relative "siamese_rescue_scraper/version"
+require_relative 'siamese_rescue_scraper/version'
 require_relative './siamese_rescue_scraper/cli'
+require_relative './siamese_rescue_scraper/cat'
---
+++ lib/siamese_rescue_scraper/cat.rb
@@ -0,0 +1,4 @@
+class SiameseRescueScraper::Cat
+  attr_accessor :name, :ID, :location, :point, :bio, :age, :requirements
+
+end
