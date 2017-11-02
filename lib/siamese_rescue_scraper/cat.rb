ddf1f28e3f9cb37a25b38fdc9410fb0b4acaee7f
Ally Kadel <akadel01@gmail.com>
2017-11-02 13:09:33 +0000

    abstract Cat .all method

--- lib/siamese_rescue_scraper/cat.rb
+++ lib/siamese_rescue_scraper/cat.rb
@@ -31,14 +31,11 @@ class SiameseRescueScraper::Cat
     #return array of Cat instances
     #scrape SR and return Cats based on data
 
-    name1, sex1, location1, point1, age1 = "Koda", "Female", "Virginia Center", "Seal", "9 Yrs 6 Mon"
-    name2, sex2, location2, point2, age2 = "Maya", "Female", "Florida", "Tortie", "9 Yrs 4 Mon"
-    cat1 = self.new(name1, sex1, location1, point1, age1)
-    cat2 = self.new(name2, sex2, location2, point2, age2)
-    puts "Siamese Cats Available:"
-    puts "1 #{cat1.name} - #{cat1.sex} - #{cat1.point} - #{cat1.age} - #{cat1.location}"
-    puts "2 #{cat2.name} - #{cat2.sex} - #{cat2.point} - #{cat2.age} - #{cat2.location}"
-    [cat1,cat2]
+    @@cats
+    @@cats.each_with_index do |cat, index|
+      puts "#{index+1} #{cat.name} - #{cat.id} - #{cat.loc}"
+    end
+
   end
 
 end
